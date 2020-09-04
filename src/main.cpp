b184009b5e76660d205d03d896204036d731a5cb
zapataramil <rzapata@fi.uba.ar>
2020-09-04 15:36:46 -0300

    funciona el metodo de ordenamiento burbujeo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-void intercambiar(int pre, int post){
+#include <iostream>
+
+void intercambiar(int & pre, int & post){
 		int aux = pre;
 		pre =post;
 		post=aux;
@@ -7,14 +9,26 @@ void intercambiar(int pre, int post){
 
 int main(){
 
-
 	int tamanioVector= 10;
 	int vector[tamanioVector];
+
+	for(int i=0; i<10 ;i++){
+		int num;
+		std::cout<<"message: ";
+		std::cin >> num;
+		vector[i]= num;
+	}
+
+
 	for(int i=0 ; i<tamanioVector; i++){
-		for (int j=0 ; j< tamanioVector -(i+1) ;j++){
-			if (vector[i]>vector[j])
-				intercambiar(vector[i],vector[j]);
+		for (int j=1 ; j< tamanioVector -i ;j++){
+			if (vector[j-1]>vector[j])
+				intercambiar(vector[j-1],vector[j]);
 		}
+	}
+
+	for(int i=0 ; i<tamanioVector; i++){
+		std::cout<< "valor "<<i<<" :"<<vector[i]<<std::endl;
 	}
 
 
